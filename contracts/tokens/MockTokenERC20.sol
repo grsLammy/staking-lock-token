52524970f90f12151f36debb631af5126419a68c
grsLammy <grspro98@gmail.com>
2022-03-24 00:19:53 +0530

    :art: better comments
    better comments

--- contracts/tokens/MockTokenERC20.sol
+++ contracts/tokens/MockTokenERC20.sol
@@ -5,15 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MockTokenERC20 is ERC20, Ownable {
-    address public owner;
 
     constructor() ERC20("ERC20 MockToken", "MockToken") {
-        owner = msg.sender;
         uint256 totalSupply = 1000000000 ether; // 1 billion supply
         _mint(msg.sender, totalSupply);
     }
 
-    //funtion to mint more tokens
+    // Funtion to mint more tokens
     function mintERC20(
         address account, 
         uint256 amount
