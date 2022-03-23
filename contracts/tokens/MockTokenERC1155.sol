498d32fb78c5b5dc0f251327a882a29b4253b0ad
grsLammy <grspro98@gmail.com>
2022-03-24 00:20:53 +0530

    :art: better comments
    better comments

--- contracts/tokens/MockTokenERC1155.sol
+++ contracts/tokens/MockTokenERC1155.sol
@@ -8,14 +8,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MockTokenERC1155 is ERC1155, Ownable {
     uint256 public constant TOKEN_1 = 1;
     uint256 public constant  TOKEN_2 = 2;
-    uint256 totalSupply = 1000000000 ether; // 1 billion supply
+    uint256 totalSupply = 1000000000 ether;    // 1 billion supply
 
     constructor() ERC1155("https://api.mysite.com/tokens/{id}") {
-        _mint(msg.sender, TOKEN_1, totalSupply, ""); //mint 1 billion token for TOKEN_1 (fungible token)
-        _mint(msg.sender, TOKEN_2, 1, ""); //mint only 1 token for TOKEN_2 (non-fungible token)
+        _mint(msg.sender, TOKEN_1, totalSupply, "");    // Mint 1 billion token for TOKEN_1 (fungible token)
+        _mint(msg.sender, TOKEN_2, 1, "");    // Mint only 1 token for TOKEN_2 (non-fungible token)
     }
 
-    // function to mint ERC1155 token
+    // Function to mint ERC1155 token
     function mintMockERC1155(
         address account,
         uint256 id,
@@ -25,7 +25,7 @@ contract MockTokenERC1155 is ERC1155, Ownable {
         _mint(account, id, amount, data);
     }
 
-    // function to mint ERC1155 token in batch
+    // Function to mint ERC1155 token in batch
     function mintBatchMockERC1155(
         address to,
         uint256[] memory ids,
